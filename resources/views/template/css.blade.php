a7e0be3321d387d7082895d04073d08cc26e6d5d
setiaendra18 <setiaendra18@gmail.com>
2021-01-16 23:56:38 +0700

    template

---
+++ resources/views/template/css.blade.php
@@ -0,0 +1,17 @@
+  <!-- Favicons -->
+  <link href="{{url('assets/img/favicon.png')}}" rel="icon">
+  <link href="{{url('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{url('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/aos/aos.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{url('assets/css/style.css')}}" rel="stylesheet">
